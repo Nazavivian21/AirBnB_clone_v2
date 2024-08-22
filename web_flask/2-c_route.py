4de718ccd7c55d27a12433b7cf9c93d707663a6c
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-22 11:50:51 +0100

    Starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return f"C {text.replace('_', ' ')}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
