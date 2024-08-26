7bd9f03c90e413ba770aa3b8d894e30230618842
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-08-26 10:06:36 +0100

    Make corrections to the file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,8 +33,9 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('templates/5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
